eb34360265c55c99006316f0d86a885ca660a3d7
anjali-anand <67236393+anjali-anand@users.noreply.github.com>
2020-06-28 18:59:48 +0530

    This is the login page for FB along with another page that enables you to create an FB account.

---
+++ app/src/main/java/com/example/facebook_login/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.facebook_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
